e8c2c415579a1c468dfe04aeac3a131519b783ac
MarceloRPC <marcelo.paiva@argobr.com>
2025-02-22 15:50:34 -0300

    create theme and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import { GlobalStyles } from "./styles/globalStyles";
+
 function App() {
   return (
     <>
-      
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+      </ThemeProvider>
     </>
   )
 }
